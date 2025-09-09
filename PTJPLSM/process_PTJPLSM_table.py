b0702f4d089f324fc157a7e15de1cd08e658ca99
Gregory Halverson <gregory.halverson@gmail.com>
2025-09-08 17:43:55 -0700

    daylight upscaling

--- PTJPLSM/process_PTJPLSM_table.py
+++ PTJPLSM/process_PTJPLSM_table.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 
 def process_PTJPLSM_table(
         input_df: DataFrame,
-        upscale_to_daily: bool = UPSCALE_TO_DAILY,
+        upscale_to_daily: bool = UPSCALE_TO_DAYLIGHT,
         regenerate_net_radiation: bool = False
         ) -> DataFrame:
     """
@@ -245,7 +245,7 @@ def process_PTJPLSM_table(
         RH=RH,
         soil_moisture=soil_moisture,
         Rn_Wm2=Rn_Wm2,
-        Rn_daily_Wm2=Rn_daily_Wm2,
+        Rn_daylight_Wm2=Rn_daily_Wm2,
         Topt_C=Topt_C,
         fAPARmax=fAPARmax,
         canopy_height_meters=canopy_height_meters,
@@ -256,7 +256,7 @@ def process_PTJPLSM_table(
         SWin_Wm2=SWin_Wm2,
         time_UTC=time_UTC,
         regenerate_net_radiation=regenerate_net_radiation,
-        upscale_to_daily=upscale_to_daily
+        upscale_to_daylight=upscale_to_daily
     )
 
     output_df = input_df.copy()
